9c65da306535a5092a2608a452ccd9c4f163e4d1
Chris Love <cjlove@fir.san.rr.com>
2013-08-08 21:16:32 -0700

    find_all()

--- Strings/strings.cpp
+++ Strings/strings.cpp
@@ -3,8 +3,20 @@
 //
 #include <string>
 #include <iostream>
+#include <vector>
+#include <list>
 
 using namespace std;
+
+template<typename C, typename V>
+vector<typename C::iterator> find_all(C &c, V v)
+{
+    vector<typename C::iterator> res;
+    for (auto p = c.begin(); p!= c.end(); ++p) {
+        if (*p == v) res.push_back(p);
+    }
+    return res;
+}
 
 int main(int argc, char **argv)
 {
@@ -23,5 +35,29 @@ int main(int argc, char **argv)
     } else {
         cout << "No match\n";
     }
+
+    string g { "abc123abc666abc" };
+
+    for (auto p: find_all(g,'a') ) {
+        if (*p != 'a')
+            cout << "Bug!\n";
+    }
+    cout << "Done\n";
+
+    list<double> ld { 1.1, 2.2, 3.3, 4.4, 5.5 };
+    for (auto p: find_all(ld, 1.1)) {
+        if (*p != 1.1) {
+            cout << "Double bug\n";
+        }
+    }
+    cout << "Double done\n";
+
+    vector<string> vs { "red", "green", "green", "orange", "red" };
+    for (auto p: find_all(vs, "green")) {
+        if (*p != "green" ) {
+            cout << "String bug\n";
+        }
+    }
+    cout << "String done\n";
 
 }
