cded29f47009647d38d7ca6a2de285d6a30a0295
Chris Love <cjlove@fir.san.rr.com>
2013-08-20 19:15:04 -0700

    Alternate enum class debug code approach

--- Structures/enums.cpp
+++ Structures/enums.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 // Enum class definition
 enum class Colors : int { red, green, blue, yellow, brown, black };
@@ -31,6 +32,24 @@ public:
 
 };
 
+// Alternate approach to bundling debug code with enum class
+// Define to_string() for the enum class in a namespace and use
+// Argument Dependent Lookup to find the to_string() in the Bar namespace
+// supporting the Baz type
+
+namespace Bar {
+    enum class Baz : int { red,green,blue };
+
+    std::string to_string(Baz baz) {
+        switch(baz) {
+        case Baz::red: return "Red";
+        case Baz::green: return "Green";
+        case Baz::blue: return "Blue";
+        default: return "Invalid";
+        }
+    }
+}
+
 
 int main()
 {
@@ -41,4 +60,8 @@ int main()
     c = Colors::black;
 
     std::cout << c << "\n";
+
+    Bar::Baz b = Bar::Baz::green;
+
+    std::cout << to_string(b) << "\n";
 }
